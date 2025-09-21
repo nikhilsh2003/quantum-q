212ad425ec97666e8545054990271d62bc84263b
Nikhil Sharma <nikhilsharmash65@gmail.com>
2025-09-21 19:49:36 +0530

    feat: Initialise Spring Boot application

---
+++ src/main/java/com/quantum_q/quantum_q/QuantumQApplication.java
@@ -0,0 +1,13 @@
+package com.quantum_q.quantum_q;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantum_q/quantum_q/QuantumQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantum_q.quantum_q;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
